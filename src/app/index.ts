0bc0ae5ddaf0974f83d6240cdf3fb34c679ce32f
Stasy <daara-md@yandex.ru>
2023-04-04 00:00:49 +0300

    git add
    Final

---
+++ src/app/index.ts
@@ -0,0 +1,10 @@
+const commentSystem = new CommentSystem() // Initialization of the commenting system
+commentSystem.createUser("Максим Авдеенко", "../src/img/Maks user.png")
+
+
+
+
+
+        
+
+
